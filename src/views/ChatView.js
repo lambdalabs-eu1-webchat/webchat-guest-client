a52203c3afb81ac96b53d2a525e8c0193a4990cf
conconrob <connortprobertson@gmail.com>
2019-05-04 22:43:46 +0100

    getting the tickets of a user

--- src/views/ChatView.js
+++ src/views/ChatView.js
@@ -3,6 +3,10 @@ import socketIOClient from 'socket.io-client';
 import { DOMAIN, SOCKET } from '../utils/constants';
 
 class ChatView extends React.Component {
+  state = {
+    chat_id: null,
+    tickets: [],
+  };
   componentDidMount() {
     const socket = socketIOClient(DOMAIN);
     socket.on(SOCKET.connection, () => {
@@ -11,7 +15,12 @@ class ChatView extends React.Component {
       socket.emit(SOCKET.login, this.props.token);
     });
     socket.on(SOCKET.chat_log, chatLog => {
+      // replace the old chatlog with the new one
+      this.setState({ chat_id: chatLog._id, tickets: chatLog.tickets });
       console.log(chatLog);
+    });
+    socket.on(SOCKET.message, message => {
+      // place the message in the latest ticket messages
     });
   }
 
