d2f5a5e5016980e597cb9d480a6d20cd7dcb47f9
conconrob <connortprobertson@gmail.com>
2019-05-04 18:38:18 +0100

    basic routing for login view or chat view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function App() {
-  return <div className='App' />;
+import LoginView from './views/LoginView';
+import ChatView from './views/ChatView';
+
+class App extends React.Component {
+  state = {
+    user: null,
+    token: null,
+  };
+  render() {
+    const { user, token } = this.state;
+    if (!token) return <LoginView />;
+    return <ChatView />;
+  }
 }
 
 export default App;
---
+++ src/views/LoginView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LoginView extends React.Component {
+  render() {
+    return <h1>LOGIN</h1>;
+  }
+}
+
+export default LoginView;
