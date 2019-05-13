7f769b55f31db49a70bd59765f163cabbde17631
Pav0l <pavol.karas@gmail.com>
2019-05-13 14:35:21 +0200

    Get user credentials and populate input fields with them

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
+import jwt from 'jsonwebtoken';
 // components
 import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
@@ -18,6 +19,22 @@ class LoginView extends React.Component {
     nameInput: '',
     passcodeInput: '',
   };
+
+  componentDidMount() {
+    // get the fragment identifier of the URL
+    const location = window.location.hash;
+    if (location) {
+      // separate the hash
+      const hashString = location.substring(1);
+      // decode jwt
+      const decoded = jwt.decode(hashString);
+      // update input field values
+      this.setState({
+        nameInput: decoded.name,
+        passcodeInput: decoded.passcode,
+      });
+    }
+  }
 
   handleChange = (event, input) => {
     const value = event.target.value;
@@ -43,42 +60,42 @@ class LoginView extends React.Component {
     const { nameInput, passcodeInput } = this.state;
     return (
       <StyledLoginView>
-          <div className="login"> 
+        <div className="login">
           <h1>LOGIN</h1>
-        <form className="container">
-          <FormControl margin='normal' required fullWidth>
-            <InputLabel htmlFor='name'>Name</InputLabel>
-            <Input
-              id='name'
-              name='name'
-              autoComplete='name'
-              autoFocus
-              value={nameInput}
-              onChange={event => this.handleChange(event, 'nameInput')}
-            />
-          </FormControl>
-          <FormControl margin='normal' required fullWidth>
-            <InputLabel htmlFor='passcode'>Passcode</InputLabel>
-            <Input
-              name='passcode'
-              type='passcode'
-              id='passcode'
-              autoComplete='current-passcode'
-              value={passcodeInput}
-              onChange={event => this.handleChange(event, 'passcodeInput')}
-            />
-          </FormControl>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-            onClick={this.login}
-          >
-            Sign in
-          </Button>
-        </form>
-          </div>
+          <form className="container">
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <Input
+                id="name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+                value={nameInput}
+                onChange={event => this.handleChange(event, 'nameInput')}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="passcode">Passcode</InputLabel>
+              <Input
+                name="passcode"
+                type="passcode"
+                id="passcode"
+                autoComplete="current-passcode"
+                value={passcodeInput}
+                onChange={event => this.handleChange(event, 'passcodeInput')}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={this.login}
+            >
+              Sign in
+            </Button>
+          </form>
+        </div>
       </StyledLoginView>
     );
   }
