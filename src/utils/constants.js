729d58a27da4545530ce5d0cfa447015deea560b
Pav0l <pavol.karas@gmail.com>
2019-05-15 16:16:17 +0200

    fix domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const DOMAIN = 'https://web-chat-labs.herokuapp.com';
-export const DOMAIN = 'http://localhost:7000';
+export const DOMAIN = 'https://web-chat-labs.herokuapp.com';
+// export const DOMAIN = 'http://localhost:7000';
 
 export const PATH = {
   users: '/api/users',
