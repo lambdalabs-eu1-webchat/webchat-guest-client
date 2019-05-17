e2662b118418c17340e112190ff96da4d8d7f835
Pav0l <pavol.karas@gmail.com>
2019-05-17 13:08:56 +0200

    Remove unused variables/files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,8 @@
-export const DOMAIN = 'https://web-chat-labs.herokuapp.com';
-// export const DOMAIN = 'http://localhost:7000';
+// export const DOMAIN = 'https://web-chat-labs.herokuapp.com';
+export const DOMAIN = 'http://localhost:7000';
 
 export const PATH = {
   users: '/api/users',
-  auth: '/api/auth',
-  hotel: '/api/hotel',
-  subscribe: '/api/subscription',
 };
 
 export const SOCKET = {
