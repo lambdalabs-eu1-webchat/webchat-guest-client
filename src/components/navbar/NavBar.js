210bace483bf7d847cce4524f08307dd77bcc563
Pav0l <pavol.karas@gmail.com>
2019-05-17 13:28:34 +0200

    Make logo wide on navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,14 +3,10 @@ import styled from 'styled-components';
 import logo from './logo2.svg';
 import theme from '../../theme/styledTheme';
 
-const NavBar = props => {
+const NavBar = () => {
   return (
-    <StyledNav className="nav-wrapper navy darken-2 hide-on-print">
-      <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />{' '}
-        </header>
-      </div>
+    <StyledNav>
+      <img src={logo} alt="logo" />
     </StyledNav>
   );
 };
@@ -19,30 +15,5 @@ export default NavBar;
 const StyledNav = styled.nav`
   background: ${theme.color.secondaryPurple};
   padding: 1rem 2.5rem;
-  text-transform: uppercase;
-  a {
-    text-decoration: none;
-    font-size: ${theme.fontSize.xs};
-    color: ${theme.color.white};
-  }
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-
-    img {
-      border-radius: 0;
-      width: 60%;
-    }
-  }
-  .tickets {
-    color: ${theme.color.accentGreen};
-  }
-  .active {
-    color: ${theme.color.white};
-  }
-  .brand-logo {
-    z-index: 5;
   }
 `;
