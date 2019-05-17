33f1f0203c86e210c3df5d8ade7f86a143b5a052
Pav0l <pavol.karas@gmail.com>
2019-05-17 14:14:31 +0200

    Clean up message bubble

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
-import theme from '../theme/styledTheme'
+import theme from '../theme/styledTheme';
 
 function Message({ message, user_id }) {
-  if (!message.sender) debugger;
+  if (!message.sender);
   return (
     <StyledMessage left={user_id === message.sender.id}>
       <span>{message.sender.name} : </span>
-      <div className='bubble-container'>
-        <span className='bubble me'>{message.text}</span>
+      <div>
+        <span>{message.text}</span>
       </div>
     </StyledMessage>
   );
@@ -27,30 +27,17 @@ Message.propTypes = {
 };
 
 const StyledMessage = styled.div`
-margin: 2rem;
-position: relative;
-max-width: 50%;
-border-radius:1em;
-padding: 2rem;
-width:70em;
-overflow: scroll;
+  margin: 1rem 0;
+  max-width: 50%;
+  border-radius:1em;
+  padding: 1rem;
 
-
-${props => (props.left ? `background:${theme.color.footerText};` : `background:${theme.color.accentPurple};`)}
-${props => (props.left ? `text-align: right;` : 'text-align: left;')}
-${props => (props.left ? `margin-left:auto;` : 'margin-right:auto;')}
-&:after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  width: 0.2rem;
-  height: -20.2em;
-  border: 1em 3em 1em 3em ;
-  border-bottom:20em;
-  margin-right: -0.1em;
-  margin-bottom: -0.562em;
-
-}
+  ${props =>
+    props.left
+      ? `background:${theme.color.footerText};`
+      : `background:${theme.color.accentPurple};`}
+  ${props => (props.left ? `text-align: right;` : 'text-align: left;')}
+  ${props => (props.left ? `margin-left: auto;` : 'margin-right: auto;')}
 `;
-// border-right: 0;
+
 export default Message;
