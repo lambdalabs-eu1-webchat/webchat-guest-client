1da63c719dcf1cb130525c32851c1a04869a5c69
Pav0l <pavol.karas@gmail.com>
2019-05-21 17:23:25 +0200

    Update request with auth header

--- src/requests/ajax.js
+++ src/requests/ajax.js
@@ -6,12 +6,19 @@ export async function loginRequest(name, passcode) {
   // login and return the user and token
   const res = await axios.post(`${DOMAIN}${PATH.users}/chat`, {
     name,
-    passcode,
+    passcode
   });
   return res.data; // {user, token}
 }
 
-export async function getHotel(hotel_id) {
-  const res = await axios.get(`${DOMAIN}${PATH.hotel}/${hotel_id}`);
+export async function getHotel(hotel_id, token) {
+  const axiosConfig = {
+    headers: { Authorization: token }
+  };
+
+  const res = await axios.get(
+    `${DOMAIN}${PATH.hotel}/${hotel_id}/chat`,
+    axiosConfig
+  );
   return res.data;
 }
